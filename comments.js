259f624927b97e623e9897c5fd287e9602ea03aa
Tobias Kvist Hjuler <84204734+Tohjuler@users.noreply.github.com>
2023-11-07 08:25:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,34 @@
-// Create a simple web server
+// Create web server
 
-// Import the express module
+// Import modules
 const express = require('express');
+const router = express.Router();
+const db = require('../db');
+const { check, validationResult } = require('express-validator');
+const { v4: uuidv4 } = require('uuid');
 
-// Create a new web server
+// Get all comments
+router.get('/', (req, res) => {
+    const sql = 'SELECT * FROM comments';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+// Get comment by id
+router.get('/:id', (req, res) => {
+    const sql = `SELECT * FROM comments WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+// Start web server
 const app = express();
+app.use(express.json());
+app.use('/comments', router);
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
